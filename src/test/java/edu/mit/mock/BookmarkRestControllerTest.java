4e0ab6d8026b0ba384bbefb51fcb5739c60d9800
Osman Din <osmandin@gmail.com>
2018-01-18 14:28:42 -0500

    Add sample tests

--- src/test/java/edu/mit/mock/BookmarkRestControllerTest.java
+++ src/test/java/edu/mit/mock/BookmarkRestControllerTest.java
@@ -1,4 +1,121 @@
 package edu.mit.mock;
 
+
+import edu.mit.entity.DepartmentsForm;
+import edu.mit.entity.UsersForm;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.mock.http.MockHttpOutputMessage;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+
+/**
+ * @author Josh Long
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@WebAppConfiguration
 public class BookmarkRestControllerTest {
+
+
+    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(),
+            Charset.forName("utf8"));
+
+
+    private MockMvc mockMvc;
+
+    private String userName = "bdussault";
+
+    private HttpMessageConverter mappingJackson2HttpMessageConverter;
+
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+
+    @Autowired
+    void setConverters(HttpMessageConverter<?>[] converters) {
+
+        this.mappingJackson2HttpMessageConverter = Arrays.asList(converters).stream()
+                .filter(hmc -> hmc instanceof MappingJackson2HttpMessageConverter)
+                .findAny()
+                .orElse(null);
+
+        assertNotNull("the JSON message converter must not be null",
+                this.mappingJackson2HttpMessageConverter);
+    }
+
+    @Before
+    public void setup() throws Exception {
+        this.mockMvc = webAppContextSetup(webApplicationContext).build();
+
+       /* this.bookmarkRepository.deleteAllInBatch();
+        this.accountRepository.deleteAllInBatch();
+
+        this.account = accountRepository.save(new Account(userName, "password"));
+        this.bookmarkList.add(bookmarkRepository.save(new Bookmark(account, "http://bookmark.com/1/" + userName, "A description")));
+        this.bookmarkList.add(bookmarkRepository.save(new Bookmark(account, "http://bookmark.com/2/" + userName, "A description")));*/
+    }
+
+  /*  @Test
+    public void userNotFound() throws Exception {
+        mockMvc.perform(post("/george/bookmarks/")
+                .content(this.json(new Bookmark()))
+                .contentType(contentType))
+                .andExpect(status().isNotFound());
+    }
+*/
+
+    @Ignore
+    @Test
+    public void createBookmark() throws Exception {
+
+
+        UsersForm user = new UsersForm();
+        user.setUsername("osman");
+        DepartmentsForm df = new DepartmentsForm();
+        df.setName("test");
+        List<DepartmentsForm> departmentList = new ArrayList<>();
+        departmentList.add(df);
+        user.setDepartmentsForms(departmentList);
+
+        this.mockMvc.perform(post("/add")
+                .contentType(contentType)
+                .content(this.json(user)))
+                .andExpect(status().isCreated());
+
+        //this.mockMvc.perform(get("/ListUsers")).andDo(print()).andExpect(status().isOk())
+          //      .andExpect(content().string(containsString("osman")));
+
+    }
+
+    protected String json(Object o) throws IOException {
+        MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
+        this.mappingJackson2HttpMessageConverter.write(
+                o, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
+        return mockHttpOutputMessage.getBodyAsString();
+    }
 }
