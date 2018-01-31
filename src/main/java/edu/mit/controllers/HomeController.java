2f8205e24f30368a50131e39730e202a0f0f9011
Osman Din <osmandin@gmail.com>
2018-01-31 11:21:14 -0500

    Add Faq page

--- src/main/java/edu/mit/controllers/HomeController.java
+++ src/main/java/edu/mit/controllers/HomeController.java
@@ -1,10 +1,14 @@
 package edu.mit.controllers;
 
+import edu.mit.entity.OrgInfo;
 import org.slf4j.Logger;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.Resource;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -14,6 +18,9 @@ public class HomeController {
 
     private final Logger logger = getLogger(this.getClass());
 
+    @Resource
+    private Environment env;
+
     /**
      * Handles all request
      * @return model and view
@@ -22,6 +29,22 @@ public class HomeController {
     public ModelAndView showItemsPage() {
         final ModelAndView modelAndView = new ModelAndView("Home");
         return modelAndView;
+    }
+
+    /**
+     * Handles all request
+     * @return model and view
+     */
+    @RequestMapping(value={"/Faq"}, method = RequestMethod.GET)
+    public ModelAndView show() {
+        final ModelAndView model = new ModelAndView("Faq");
+        OrgInfo org = new OrgInfo();
+        org.setEmail(env.getRequiredProperty("org.email"));
+        org.setPhone(env.getRequiredProperty("org.phone"));
+        org.setName(env.getRequiredProperty("org.name"));
+        org.setNamefull(env.getRequiredProperty("org.namefull"));
+        model.addObject("org", org);
+        return model;
     }
 
 }
