4d249270a5653b1f65cee317772dc4cf68dac812
Osman Din <osmandin@gmail.com>
2018-08-15 15:06:58 -0400

    Role cleanup

--- src/main/java/edu/mit/authz/Role.java
+++ src/main/java/edu/mit/authz/Role.java
@@ -2,7 +2,7 @@ package edu.mit.authz;
 
 public enum Role {
 
-    siteadmin(Constants.SITEADMIN), deptadmin (Constants.DEPTADMIN), visitor (Constants.VISITOR), donor (Constants.DONOR);
+    siteadmin(Constants.SITEADMIN), deptadmin(Constants.DEPTADMIN), visitor(Constants.VISITOR), donor(Constants.DONOR);
 
     final String role;
 
@@ -10,18 +10,7 @@ public enum Role {
         this.role = role;
     }
 
-    // for finer grained permissions
-    boolean isPermitted(final Role role) {
-
-        if (role == Role.siteadmin) {
-            return true;
-        }
-
-        return false;
-    }
-
-    // TODO decide if this should be an external class
-    public static class Constants {
+    private static class Constants {
         static final String SITEADMIN = "siteadmin";
         static final String DEPTADMIN = "deptadmin";
         static final String VISITOR = "visitor";
