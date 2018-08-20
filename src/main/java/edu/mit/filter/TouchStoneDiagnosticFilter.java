ee90ae834fa96e9ccf5ff529dbb2dff76ddb100c
Osman Din <osmandin@gmail.com>
2018-08-20 15:05:12 -0400

    Refactor filter

---
+++ src/main/java/edu/mit/filter/TouchStoneDiagnosticFilter.java
@@ -0,0 +1,65 @@
+package edu.mit.filter;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * Filter to set http servlet variable when in development.
+ *
+ * Could also be used for displaying variables set up by Touchstone
+ *
+ * The filter is used when Touchstone is not in effect (e.g., for local development)
+ */
+
+@Component
+@Order(1)
+//@WebFilter(urlPatterns = {"/", "/Logout", "/*", "/**", "/Admin"})
+@WebFilter
+public class TouchStoneDiagnosticFilter implements Filter {
+    private static final Logger logger = LoggerFactory.getLogger(TouchStoneDiagnosticFilter.class);
+
+    // TODO: for testing only. Remove the annotation when using in production
+    @Resource
+    private Environment env;
+
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        logger.debug("Init filter");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        logger.info("Filter:" + ((HttpServletRequest) servletRequest).getRequestURL());
+
+
+        final HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+
+        // Information from TouchStone:
+        // other Attributes are displayName, mail, nickname
+        logger.info("Touchstone Attribute {} :{}", "mail", httpServletRequest.getAttribute("mail"));
+
+
+        if (env != null && env.getRequiredProperty("testing.status").equals("true")) {
+            httpServletRequest.setAttribute("mail", "osmandin@mit.edu");
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
